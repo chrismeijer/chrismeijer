9709747fa4bb9497bd14825e466c1d0dc4718d98
Chris Meijer <chris.meijer@tyrenet.nl>
2020-02-25 16:14:45 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # My Own Website
+
+This is my repository for my own website, [chrismeijer.nl](http://chrismeijer.nl).
+
+## Installation
+
+After cloning, first run:
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./dist/**/*.html')
+        },
+        contentBase: path.resolve(__dirname, 'src'),
+        watchContentBase: true,
+        compress: true,
+        port: 3000
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', 
+                    'css-loader?url=false', 
+                    {
+                        loader: 'postcss-loader',
+                        options: {plugins: postCSSPlugins}
+                    }
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        publicPath: 'img/'
+                    }
+                }],
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            }
+        ]
+    }, 
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+}
